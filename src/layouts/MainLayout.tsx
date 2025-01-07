122532f9c250b9d41a64129b36ca8edf39eb571b
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2025-01-08 04:32:55 +0600

    layout created

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,74 @@
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+import { createElement } from "react";
+const { Header, Content, Footer, Sider } = Layout;
+
+const items = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  UserOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: createElement(icon),
+  label: `nav ${index + 1}`,
+}));
+
+const MainLayout = () => {
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+
+            height: "4rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>PH Uni</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            Main Content should be here
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          ©{new Date().getFullYear()} PH University Management System | Created
+          by Md Shakil Hossain
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
