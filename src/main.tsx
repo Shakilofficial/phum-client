be4b38aa40a8e1939e9309a7c46f12c125f37679
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2025-01-09 11:43:04 +0600

    initaliazed redux tolkit and setup with the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
+import "@fontsource/lexend/400.css";
+import "@fontsource/lexend/700.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import { store } from "./redux/store.ts";
 import router from "./routes/Routes.tsx";
-import "@fontsource/lexend/400.css";
-import "@fontsource/lexend/700.css"; 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
