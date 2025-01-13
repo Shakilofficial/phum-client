81d71bfd9c04577773fcb3337f8980eb3994fdf6
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2025-01-13 11:00:23 +0600

    send token in headers

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,4 +1,8 @@
+import { useGetAllAcademicSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
+
 const AcademicSemester = () => {
+  const { data } = useGetAllAcademicSemestersQuery(undefined);
+  console.log(data);
   return (
     <div>
       <h1>This is the AcademicSemester component</h1>
